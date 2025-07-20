a98e55e312a7f315c9f72be96a6a1eea7874daec
jres <ljresetl@gmail.com>
2025-07-20 13:51:01 +0200

    up

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-import'./js/menu.js';
+async function loadPartial(id, url) {
+  const container = document.getElementById(id);
+  if (container) {
+    const res = await fetch(url);
+    const html = await res.text();
+    container.innerHTML = html;
+  }
+}
+
+loadPartial('header', './partials/header.html');
+loadPartial('search-bar', './partials/search-bar-nav.html');
+loadPartial('filters', './partials/filters.html');
+loadPartial('movies-list', './partials/movies-list.html');
+loadPartial('block-new', './partials/block-new.html');
+loadPartial('opis-kino', './partials/opis-kino-ua.html');
+loadPartial('footer', './partials/footer.html');
